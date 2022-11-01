9b4285b9cf10b07ce29faca37a02e17b495de841
uuronn <uuronprogram@gmail.com>
2022-11-02 06:33:15 +0900

    タスクを表示

--- app.ts
+++ app.ts
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req: any, res: any) {
-  res.render("./pages/index.ejs", { title: "index title" });
+  res.render("./pages/index.ejs", {
+    title: "todo aria",
+    todos: [{ title: "title1" }, { title: "title2" }, { title: "title3" }]
+  });
 });
 
 console.log("http://localhost:" + app.listen(3000).address().port);
